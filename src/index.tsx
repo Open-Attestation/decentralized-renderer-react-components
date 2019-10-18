b7420bd0a276bc920f87de72e56b2f5d17fc8750
Nebulis <nebounet@gmail.com>
2019-10-18 18:00:53 +0800

    fix: export ObfuscatableValue, SimplePrivacyFilterBanner and types

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
+export { ObfuscatableValue } from "./components/common/ObfuscatableValue";
+export { SimplePrivacyFilterBanner } from "./components/common/SimplePrivacyFilterBanner";
 export { FramedDocumentRenderer } from "./components/renderer/FramedDocumentRenderer";
 export { LegacyFramedDocumentRenderer } from "./components/renderer/LegacyFramedDocumentRenderer";
+export * from "./types";
