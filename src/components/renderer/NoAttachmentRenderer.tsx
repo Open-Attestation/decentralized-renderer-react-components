5f3539f59a4cc7e19f7f26817e517ef8cafe3643
Raymond Yeh <ray@geek.sg>
2020-10-20 15:33:55 +0800

    fix: lint

--- src/components/renderer/NoAttachmentRenderer.tsx
+++ src/components/renderer/NoAttachmentRenderer.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Attachment } from "../../types";
 
 /**
  * Function using unsupported renderer for all kind of attachments
  */
-export function noAttachmentRenderer<D extends Document>(attachment: Attachment): React.FunctionComponent {
+export function noAttachmentRenderer<D extends Document>(): React.FunctionComponent {
   // eslint-disable-next-line react/display-name
   return () => null;
 }
