2cc3ac1509a69de833748f4db91bf15cce8d48d7
Raymond Yeh <ray@geek.sg>
2020-10-20 15:53:36 +0800

    fix: null renderer return type

--- src/components/renderer/NoAttachmentRenderer.tsx
+++ src/components/renderer/NoAttachmentRenderer.tsx
@@ -1,9 +1,6 @@
-import React from "react";
-
 /**
- * Function using unsupported renderer for all kind of attachments
+ * Function not returning any renderer for all kind of attachments
  */
-export function noAttachmentRenderer<D extends Document>(): React.FunctionComponent {
-  // eslint-disable-next-line react/display-name
-  return () => null;
+export function noAttachmentRenderer(): null {
+  return null;
 }
