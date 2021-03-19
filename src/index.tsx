9ae281092a80d9b768c351f5418aec122841c441
Nebulis <nebounet@gmail.com>
2021-03-19 15:14:58 +0800

    fix: export utils

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,6 @@ export * from "./components/renderer/FullAttachmentRenderer";
 export * from "./components/renderer/PdfRenderer";
 export * from "./components/renderer/UnsupportedRenderer";
 export * from "./types";
+export * as utils from "./utils";
 export * from "./components/frame/host.actions";
 export * from "./components/frame/frame.actions";
