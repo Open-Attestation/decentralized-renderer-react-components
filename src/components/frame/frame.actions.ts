cab3466f9f84243dc9cc882d20d7d4a5ff6759f8
eugbyte <eugenetham1994@gmail.com>
2021-04-29 14:23:02 +0800

    chore: updated typesafe action

--- src/components/frame/frame.actions.ts
+++ src/components/frame/frame.actions.ts
@@ -1,9 +1,9 @@
 // actions sent by frame to host
-import { ActionType, createStandardAction } from "typesafe-actions";
+import { ActionType, createAction } from "typesafe-actions";
 
-export const updateHeight = createStandardAction("UPDATE_HEIGHT")<number>();
-export const obfuscateField = createStandardAction("OBFUSCATE")<string>();
-export const updateTemplates = createStandardAction("UPDATE_TEMPLATES")<
+export const updateHeight = createAction("UPDATE_HEIGHT")<number>();
+export const obfuscateField = createAction("OBFUSCATE")<string>();
+export const updateTemplates = createAction("UPDATE_TEMPLATES")<
   Array<{ id: string; label: string; type: string }>
 >();
 export type FrameActions = ActionType<typeof updateHeight | typeof obfuscateField | typeof updateTemplates>;
