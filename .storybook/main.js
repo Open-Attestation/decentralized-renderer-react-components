e20a3eb15550e43adf094a5ba7fe77027c45a0bb
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-07-12 11:21:55 +0800

    fix: revert to webpack v5 and upgrade to storybook prerelease

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  core: {
+    builder: "webpack5",
+  },
   stories: ["../src/**/*.stories.@(js|mdx)"],
   addons: ["@storybook/addon-docs/preset", "@storybook/addon-actions/register", "@storybook/addon-docs/register"],
   webpackFinal: async (config) => {
