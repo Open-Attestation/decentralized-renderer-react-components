19cc930ae8ebfb32f1f67d3f66318a0055cbd904
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-02-15 11:36:33 +0800

    fix: bump storybook from 6.4 to 7.6
    npx storybook@latest upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,13 @@ const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
+  addons: ["@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
   },
+
   webpackFinal: (config) => {
     return {
       ...config,
@@ -22,5 +24,8 @@ module.exports = {
         },
       },
     };
+  },
+  docs: {
+    autodocs: true,
   },
 };
